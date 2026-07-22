531e8582e649031b2f89d1c71126bb19205fe7e3
Pedro Augusto <pe.augusto2408@gmail.com>
2026-07-22 17:09:37 -0300

    Atualização

---
+++ Front/script.js
@@ -0,0 +1,15 @@
+//Leitor de código de barras
+const codeReader = new ZXing.BrowserBarcodeReader()
+
+codeReader.decodeFromVideoDevice(null, 'camera', (result, err) => {
+
+    if (result) {
+
+        let codigo = result.text
+
+        document.getElementById("codigo").value = codigo
+
+        console.log("Código detectado:", codigo)
+    }
+})
+
