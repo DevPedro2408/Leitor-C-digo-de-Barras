82f0f57b75d38fe27e57de06a3ebc4c8728cbc38
Pedro Augusto <pe.augusto2408@gmail.com>
2026-07-25 23:11:41 -0300

    Atualização 25/07

--- Front-End/script.js
+++ Front-End/script.js
@@ -17,9 +17,69 @@ let nomeProduto = document.getElementById("nomeProduto")
 let codigo = document.getElementById("codigo")
 let Quantidade = document.getElementById("Quantidade")
 let preco = document.getElementById("preco")
+let inputsProdutos = [...document.querySelectorAll(".inputsProdutos")]
+let adicionarProdutos = document.getElementById("adicionarProdutos")
 
 let produtos = []
 
-function salvar() {
-    
+function cadastrar() {
+
+    let verificaoProdutos = produtos.find(elements => elements.codigo_barras === codigo.value)
+
+    if (verificaoProdutos) {
+        
+    }
+
+    produtos.push({
+        nome_produto: nomeProduto.value,
+        codigo_barras: Number(codigo.value),
+        Quantidade_produtos: Number(Quantidade.value),
+        preco_value: Number(preco.value)
+        }
+    )
+
+    inputsProdutos.forEach(inputs => inputs.value = "")
+    console.log(produtos)
+    adicionarAoHTML()
+}
+
+function adicionarAoHTML() {
+    adicionarProdutos.innerHTML = ""
+    let cont = 1
+
+    produtos.forEach((elementos, indice) => {
+        let addProdutos = document.createElement("tr")
+        addProdutos.innerHTML = `
+        <td>${cont++}</td>
+        <td>${elementos.nome_produto}</td>
+        <td>${elementos.codigo_barras}</td>
+        <td>${elementos.Quantidade_produtos}</td>
+        <td>${elementos.preco_value}</td>
+        <td></td>
+        `
+
+        let ultimoElemento = addProdutos.lastElementChild
+
+        const btnEditar = document.createElement("button")
+        btnEditar.classList.add("btnEditar")
+        btnEditar.innerHTML = "Editar"
+
+        const btnExcluir = document.createElement("button")
+        btnExcluir.classList.add("btnExcluir")
+        btnExcluir.innerHTML = "X"
+
+        adicionarProdutos.appendChild(addProdutos)
+        ultimoElemento.append(btnEditar, btnExcluir)
+
+        editar(elementos, btnEditar, indice)
+    })
+}
+
+function editar(ele, botaoEditar, ind) {
+    botaoEditar.addEventListener("click", () => {
+        nomeProduto.value = produtos[ind].nome_produto
+        codigo.value = produtos[ind].codigo_barras
+        Quantidade.value = produtos[ind].Quantidade_produtos
+        preco.value = produtos[ind].preco_value
+    })
 }
